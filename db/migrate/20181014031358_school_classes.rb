8ed19786778c060827e09db62510c9b0e178b239
zhifeng ou <zhifengou@hotmail.com>
2018-10-14 03:14:29 +0000

    automatically backed up by learn

--- db/migrate/20181014031358_school_classes.rb
+++ db/migrate/20181014031358_school_classes.rb
@@ -1,5 +1,8 @@
 class SchoolClasses < ActiveRecord::Migration
   def change
-    
+    create_table :school_classes do |t|
+      t.string :title
+      t.integer :room_number
+      t.timestamps null: false
   end
 end
